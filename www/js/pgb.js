cb27b43574f5d971b4404a72c16915d1ec126eea
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 17:15:48 +0200

    dodanie alertu

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -36,4 +36,5 @@ function businesscard()
 	'Email: dominik.adamski@interia.pl' +
 	'Dean\'s group: KrDUIs1011' +
 	'Field of study: applied computer science';
+	navigator.notification.alert(info);
 }
