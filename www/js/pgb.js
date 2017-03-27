60446d64a2b6b4a9feb8a0331c46f1468aee70c2
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 23:00:01 +0200

    12

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,20 +1,58 @@
-   var sensorAcc = null;
-  
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+	navigator.notification.alert(info);
+	
+}
+
+function personalData()
+{
+	info =  'Moje imie to Dominik' + '\n' +
+			'a nazwisko: Adamski \n' 
+
+	navigator.notification.alert(info);
+}
+
+function businesscard()
+{
+	info = 'PERSONAL INFORMATION \n' +
+	'Name and surname: Dominik Adamski \n' +
+	'Email: dominik.adamski@interia.pl \n' +
+	'Dean\'s group: KrDUIs1011 \n' +
+	'Field of study: applied computer science';
+	navigator.notification.alert(info)
+}
+
     document.addEventListener("deviceready", onDeviceReady, false);
- 
-    function onDeviceReady() {
-        var options = { frequency: 80 };
- 
-        sensorAcc = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, options);
+
+  function onDeviceReady() {
+        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
     }
-     
-    function accelerometerError() {
-        alert('Failed to load accelerometer!');
+
+	
+	function onSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+              'Acceleration Y: ' + acceleration.y + '\n' +
+              'Acceleration Z: ' + acceleration.z + '\n' +
+              'Timestamp: '      + acceleration.timestamp + '\n');
     }
-     
-    function accelerometerSuccess(acceleration) {        
-        var str = 'Acceleration X: ' + acceleration.x + '<br />' +
-                  'Acceleration Y: ' + acceleration.y + '<br />' +
-                  'Acceleration Z: ' + acceleration.z + '<br />';
-        $('div.result').html(str); 
+function onError() {
+        alert('onError!');
     }
+
