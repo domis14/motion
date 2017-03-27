4326714530df741b79642829866eff570ba95ec3
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 23:07:18 +0200

    12321

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,20 +39,15 @@ function businesscard()
 	navigator.notification.alert(info)
 }
 
-    document.addEventListener("deviceready", onDeviceReady, false);
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
 
-  function onDeviceReady() {
-        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-    }
-
-	
-	function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
-    }
 function onError() {
-        alert('onError!');
-    }
+    alert('onError!');
+}
 
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
