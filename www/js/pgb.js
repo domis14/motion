69068e0824c6d9c1fe8aa128ef58b6acc1dfcf7e
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 21:53:40 +0200

    dodanie zrodla pluginu

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -39,15 +39,27 @@ function businesscard()
 	navigator.notification.alert(info)
 }
 
-function onSuccess(acceleration) {
-    alert('Acceleration X: ' + acceleration.x + '\n' +
-          'Acceleration Y: ' + acceleration.y + '\n' +
-          'Acceleration Z: ' + acceleration.z + '\n' +
-          'Timestamp: '      + acceleration.timestamp + '\n');
-};
+    // Wait for PhoneGap to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-function onError() {
-    alert('onError!');
-};
+    // PhoneGap is ready
+    //
+    function onDeviceReady() {
+        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    }
 
-navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+              'Acceleration Y: ' + acceleration.y + '\n' +
+              'Acceleration Z: ' + acceleration.z + '\n' +
+              'Timestamp: '      + acceleration.timestamp + '\n');
+    }
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    }
