4887eafaab52dddbd24d6974e7e96758e8b7274d
Dominik Adamski <dominik.adamski@interia.pl>
2017-03-27 22:02:08 +0200

    dodanie zrodla pluginu

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,65 +1,20 @@
-function init() {
-	document.addEventListener("deviceready",onDeviceReady, false);
-}
-
-function onDeviceReady() {
-	navigator.notification.beep(1);
-}
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-
-function personalData()
-{
-	info =  'Moje imie to Dominik' + '\n' +
-			'a nazwisko: Adamski \n' 
-
-	navigator.notification.alert(info);
-}
-
-function businesscard()
-{
-	info = 'PERSONAL INFORMATION \n' +
-	'Name and surname: Dominik Adamski \n' +
-	'Email: dominik.adamski@interia.pl \n' +
-	'Dean\'s group: KrDUIs1011 \n' +
-	'Field of study: applied computer science';
-	navigator.notification.alert(info)
-}
-
-    // Wait for PhoneGap to load
-    //
+   var sensorAcc = null;
+  
     document.addEventListener("deviceready", onDeviceReady, false);
-
-    // PhoneGap is ready
-    //
+ 
     function onDeviceReady() {
-        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+        var options = { frequency: 80 };
+ 
+        sensorAcc = navigator.accelerometer.watchAcceleration(accelerometerSuccess, accelerometerError, options);
     }
-
-    // onSuccess: Get a snapshot of the current acceleration
-    //
-    function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
+     
+    function accelerometerError() {
+        alert('Failed to load accelerometer!');
     }
-
-    // onError: Failed to get the acceleration
-    //
-    function onError() {
-        alert('onError!');
+     
+    function accelerometerSuccess(acceleration) {        
+        var str = 'Acceleration X: ' + acceleration.x + '<br />' +
+                  'Acceleration Y: ' + acceleration.y + '<br />' +
+                  'Acceleration Z: ' + acceleration.z + '<br />';
+        $('div.result').html(str); 
     }
